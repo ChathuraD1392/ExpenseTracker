b059d790284e4889525359a0a916b7a4e246e2a1
ChathuraD1392 <chathuradilshan1392@gmail.com>
2025-03-18 22:23:29 +0530

    intstall dependencies and create routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,46 @@
 import "./App.css";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import LogIn from "./pages/Auth/LogIn";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expences from "./pages/Dashboard/Expences";
 
 function App() {
   return (
     <>
       <div>
-        <h2 className="text-amber-300">App</h2>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Root />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/dashboard" element={<Home />} />
+            <Route path="/income" element={<Income />} />
+            <Route path="/expences" element={<Expences />} />
+          </Routes>
+        </Router>
       </div>
     </>
   );
 }
 
 export default App;
+
+const Root = () => {
+  // check if the token exists in the local storage
+
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  // redirect to the dashboard if authenticated or otherwise  login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="login" />
+  );
+};
